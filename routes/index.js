cf3665dece0fa7cacfbc25cd774d84987a102927
DTAJ095 <albanjaures26@gmail.com>
2024-08-06 14:39:16 +0100

    added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
-import FilesController from '../controllers/FilesController.js';
-import UsersController from '../controllers/UsersController.js';
-import AuthController from '../controllers/AuthController.js';
 
 
 const router = express.Router();
@@ -16,41 +13,6 @@ function routeController(app) {
 
     router.get('/stats', (req, res) => {
         AppController.getStats(req, res);
-    });
-
-    router.post('/users', (req, res) => {
-        UsersController.postNew(req, res);
-    });
-
-    router.get('/connect', (req, res) => {
-        AuthController.connect(req, res);
-    });
-
-    router.get('/disconnect', (req, res) => {
-        AuthController.disconnect(req, res);
-    });
-
-    router.get('/users/me', (req, res) => {
-        UsersController.getMe(req, res);
-    });
-
-    router.post('/files', (req, res) => {
-        FilesController.postUpload(req, res);
-    });
-    router.get('/files/:id', (req, res) => {
-        FilesController.getShow(req, res);
-    });
-    router.get('/files', (req, res) => {
-        FilesController.getIndex(req, res);
-    });
-    router.put('/files/:id/publish', (req, res) => {
-        FilesController.putPublish(req, res);
-    });
-    router.put('/files/:id/unpublish', (req, res) => {
-        FilesController.putUnpublish(req, res);
-    });
-    router.get('/files/:id/data', (req, res) => {
-        FilesController.getFile(req, res);
     });
 };
 
