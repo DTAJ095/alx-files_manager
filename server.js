74eb03a00770882c9516a11ac53483a5f8129c7d
DTAJ095 <albanjaures26@gmail.com>
2024-08-06 13:00:07 +0100

    added

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import routes from './routes/index.js';
+import controllerRouting from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
-routes(app);
+controllerRouting(app);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+export default app;
