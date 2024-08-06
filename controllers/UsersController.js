1dd1601b9d08221e23680ffe8df3b14ff383ea61
DTAJ095 <albanjaures26@gmail.com>
2024-08-06 08:11:21 -0700

    added

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,21 +3,20 @@ import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-
 class UsersController {
-    static async postNew(req, res) {
-        const { email, password } = req.body;
-        if (!email) return res.status(400).send({ error: 'Missing email' });
-        if (!password) return res.status(400).send({ error: 'Missing password' });
-    
-        const userExists = await dbClient.db.collection('users').findOne({ email });
-        if (userExists) return res.status(400).send({ error: 'Already exist' });
-    
-        const hashedPassword = sha1(password);
-        const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
-        const user = { id: result.insertedId, email };
-        res.status(201).send(user);
-    }
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+
+    const userExists = await dbClient.db.collection('users').findOne({ email });
+    if (userExists) return res.status(400).send({ error: 'Already exist' });
+
+    const hashedPassword = sha1(password);
+    const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+    const user = { id: result.insertedId, email };
+    return res.status(201).send(user);
+  }
 }
 
 export default UsersController;
