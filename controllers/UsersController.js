fc8c05dd5aa62d9a012fb0fa8ca6b085700f8feb
DTAJ095 <albanjaures26@gmail.com>
2024-08-06 15:53:28 +0100

    added

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,7 @@
-import dbClient from "../utils/db";
 import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
-import userUtils from '../utils/user';
-import queue from 'bull';
-
-const userQueue = new queue('userQueue');
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 
 class UsersController {
@@ -12,33 +9,13 @@ class UsersController {
         const { email, password } = req.body;
         if (!email) return res.status(400).send({ error: 'Missing email' });
         if (!password) return res.status(400).send({ error: 'Missing password' });
-        
-        userEmail = await dbClient.userCollection.findOne({ email });
-        if (userEmail) return res.status(400).send({ error: 'Already exist' });
-
-        const sha1Password = sha1(password);
-        let user; 
-        try {
-            user = await dbClient.userCollection.insertOne({ email, password: sha1Password });
-        } catch (error) {
-            return res.status(500).send({ error: 'Error creating user' });
-        }
-
-        const newUser = {
-            id: user.insertedId,
-            email,
-        };
-        await userQueue.add({ userId: newUser.id.toString() });
-        return res.status(201).send(newUser);
-    }
-
-    static async getMe(req, res) {
-        const { userId } = await userUtils.getUserIdAndKey(req);
-        const user = await userUtils.findUser({ _id: ObjectId(userId) });
-        if (!user) return res.status(401).send({ error: 'Unauthorized' });
-        delete user.password;
-        return res.status(200).send(user);
+    
+        const userExists = await dbClient.db.collection('users').findOne({ email });
+        if (userExists) return res.status(400).send({ error: 'Already exist' });
+    
+        const hashedPassword = sha1(password);
+        const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+        const user = { id: result.insertedId, email };
+        res.status(201).send(user);
     }
 }
-
-export default UsersController;
