000bae6a6a757625a6e95331bee515ea23548263
DTAJ095 <albanjaures26@gmail.com>
2024-08-07 12:29:39 -0700

    added

--- tests/test_dbClient.test.js
+++ tests/test_dbClient.test.js
@@ -1,40 +1,40 @@
-import { expect } from "chai";
-import chaiHttp from "chai-http";
-import dbClient from "../utils/db";
+import { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import dbClient from '../utils/db';
 
 chai.use(chaiHttp);
 
-describe('Testing MongoDB client', function() {
-  Before(async function() {
+describe('testing MongoDB client', () => {
+  Before(async () => {
     await dbClient.db.collection('users').deleteMany({});
     await dbClient.db.collection('files').deleteMany({});
   });
-  after(async function() {
+  after(async () => {
     await dbClient.db.collection('users').deleteMany({});
     await dbClient.db.collection('files').deleteMany({});
   });
-  it('Testing isAlive() function', function() {
+  it('testing isAlive() function', () => {
     expect(dbClient.isAlive()).to.equal(true);
   });
 
-  it('Testing nbUsers() function', async function() {
+  it('testing nbUsers() function', async () => {
     const nbUsers = await dbClient.nbUsers();
     expect(nbUsers).to.be.a('number');
   });
 
-  it('Testing nbFiles() function', async function() {
+  it('testing nbFiles() function', async () => {
     const nbFiles = await dbClient.nbFiles();
     expect(nbFiles).to.be.a('number');
   });
 
-  it('Counting number of users', async function() {
+  it('counting number of users', async () => {
     await dbClient.db.collection('users').insertOne({ email: 'aljsdkjas@gmail.com', password: 'mypasss2365' });
     await dbClient.db.collection('users').insertOne({ email: 'uoisdoof@gmail.com', password: 'mypasss78895' });
     const nbUsers = await dbClient.nbUsers();
     expect(nbUsers).to.equal(2);
   });
 
-  it('Counting number of files', async function() {
+  it('counting number of files', async () => {
     await dbClient.db.collection('files').insertOne({ name: 'file1', type: 'type1', parentId: 'parent1' });
     await dbClient.db.collection('files').insertOne({ name: 'file2', type: 'type2', parentId: 'parent2' });
     const nbFiles = await dbClient.nbFiles();
